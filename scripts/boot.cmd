36a5684ec5d78970bd62fdab3b871cde08cb4ac6
Juan G. Victores <jcgvicto@gmail.com>
2026-05-19 19:41:24 +0200

    rm scripts/boot.cmd

--- scripts/boot.cmd
+++
@@ -1,5 +0,0 @@
-echo "Loading Kernel (Dynamic PARTUUID)..."
-setenv bootargs console=tty1 root=PARTUUID=95d7ac2c-02 rw rootwait panic=10 loglevel=3
-load mmc 0:1 0x42000000 zImage
-load mmc 0:1 0x43000000 sun8i-a33-q8-tablet.dtb
-bootz 0x42000000 - 0x43000000
